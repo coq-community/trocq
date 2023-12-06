a9051c69656cdc89af00be3367fdf50027dc773b
Enzo Crance <enzo.crance@inria.fr>
2023-12-06 09:40:19 +0100

    :page_facing_up: License on vector ~ tuple example file

--- examples/Vector_tuple.v
+++ examples/Vector_tuple.v
@@ -1,3 +1,16 @@
+(*****************************************************************************)
+(*                            *                    Trocq                     *)
+(*  _______                   *           Copyright (C) 2023 MERCE           *)
+(* |__   __|                  *    (Mitsubishi Electric R&D Centre Europe)   *)
+(*    | |_ __ ___   ___ __ _  *       Cyril Cohen <cyril.cohen@inria.fr>     *)
+(*    | | '__/ _ \ / __/ _` | *       Enzo Crance <enzo.crance@inria.fr>     *)
+(*    | | | | (_) | (_| (_| | *   Assia Mahboubi <assia.mahboubi@inria.fr>   *)
+(*    |_|_|  \___/ \___\__, | ************************************************)
+(*                        | | * This file is distributed under the terms of  *)
+(*                        |_| * GNU Lesser General Public License Version 3  *)
+(*                            * see LICENSE file for the text of the license *)
+(*****************************************************************************)
+
 From Coq Require Import ssreflect.
 From Coq Require Import Vectors.VectorDef.
 From Trocq Require Import Trocq.
