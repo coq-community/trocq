e0708188ab0541b7f3df82a2bb13ba8620f95480
Cyril Cohen <CohenCyril@users.noreply.github.com>
2024-04-22 14:56:48 +0200

    Better examples about reduction modulo p (#36)
    * Better examples about reduction modulo p
    
    ---------
    
    Co-authored-by: Assia Mahboubi <Assia.Mahboubi@inria.fr>

--- examples/flt3_step.v
+++ examples/flt3_step.v
@@ -20,9 +20,13 @@ Declare Scope int_scope.
 Delimit Scope int_scope with int.
 Delimit Scope int_scope with ℤ.
 Local Open Scope int_scope.
-Declare Scope Zmod7_scope.
-Delimit Scope Zmod7_scope with Zmod7.
-Local Open Scope Zmod7_scope.
+Declare Scope Zmod9_scope.
+Delimit Scope Zmod9_scope with Zmod9.
+Local Open Scope Zmod9_scope.
+
+Definition unop_param {X X'} RX {Y Y'} RY
+   (f : X -> Y) (g : X' -> Y') :=
+  forall x x', RX x x' -> RY (f x) (g x').
 
 Definition binop_param {X X'} RX {Y Y'} RY {Z Z'} RZ
    (f : X -> Y -> Z) (g : X' -> Y' -> Z') :=
@@ -33,89 +37,61 @@ We setup an axiomatic context in order not to develop
 arithmetic modulo in Coq/HoTT.
 **)
 Axiom (int@{i} : Type@{i}) (zero : int) (add : int -> int -> int)
-      (mul : int -> int -> int) (one : int).
+      (mul : int -> int -> int) (one : int)
+      (mod3 : int -> int).
 Axiom (addC : forall m n, add m n = add n m).
-Axiom (Zmod7 : Type) (zerop : Zmod7) (addp : Zmod7 -> Zmod7 -> Zmod7)
-      (mulp : Zmod7 -> Zmod7 -> Zmod7) (onep : Zmod7).
-Axiom (modp : int -> Zmod7) (reprp : Zmod7 -> int)
-      (reprpK : forall x, modp (reprp x) = x).
+Axiom (Zmod9 : Type) (zerop : Zmod9) (addp : Zmod9 -> Zmod9 -> Zmod9)
+      (mulp : Zmod9 -> Zmod9 -> Zmod9) (onep : Zmod9).
+Axiom (modp : int -> Zmod9) (reprp : Zmod9 -> int)
+      (reprpK : forall x, modp (reprp x) = x)
+      (modp3 : Zmod9 -> Zmod9).
 
 Definition eqmodp (x y : int) := modp x = modp y.
 
 (* for now translations need the support of a global reference: *)
-Definition eq_Zmod7 (x y : Zmod7) := (x = y).
-Arguments eq_Zmod7 /.
+Definition eq_Zmod9 (x y : Zmod9) := (x = y).
+Arguments eq_Zmod9 /.
 
 Notation "0" := zero : int_scope.
-Notation "0" := zerop : Zmod7_scope.
+Notation "0" := zerop : Zmod9_scope.
 Notation "1" := one : int_scope.
-Notation "1" := onep : Zmod7_scope.
-Notation "x == y" := (eqmodp x%int y%int)
-  (format "x  ==  y", at level 70) : int_scope.
+Notation "1" := onep : Zmod9_scope.
 Notation "x + y" := (add x%int y%int) : int_scope.
-Notation "x + y" := (addp x%Zmod7 y%Zmod7) : Zmod7_scope.
+Notation "x + y" := (addp x%Zmod9 y%Zmod9) : Zmod9_scope.
 Notation "x * y" := (mul x%int y%int) : int_scope.
-Notation "x * y" := (mulp x%Zmod7 y%Zmod7) : Zmod7_scope.
-Notation "m ²" := (m * m)%int (at level 2) : int_scope.
-Notation "m ²" := (m * m)%Zmod7 (at level 2) : Zmod7_scope.
+Notation "x * y" := (mulp x%Zmod9 y%Zmod9) : Zmod9_scope.
+Notation not A := (A -> Empty). 
+Notation "m ^ 2" := (m * m)%int (at level 2) : int_scope.
+Notation "m ^ 2" := (m * m)%Zmod9 (at level 2) : Zmod9_scope.
 Notation "m ³" := (m * m * m)%int (at level 2) : int_scope.
-Notation "m ³" := (m * m * m)%Zmod7 (at level 2) : Zmod7_scope.
+Notation "m ³" := (m * m * m)%Zmod9 (at level 2) : Zmod9_scope.
+Notation "m % 3" := (mod3 m)%int (at level 2) : int_scope.
+Notation "m % 3" := (modp3 m)%Zmod9 (at level 2) : Zmod9_scope.
 Notation "x ≡ y" := (eqmodp x%int y%int)
   (format "x  ≡  y", at level 70) : int_scope.
 Notation "x ≢ y" := (not (eqmodp x%int y%int))
   (format "x  ≢  y", at level 70) : int_scope.
 Notation "x ≠ y" := (not (x = y)).
-Notation "ℤ/7ℤ" := Zmod7.
+Notation "ℤ/9ℤ" := Zmod9.
 Notation ℤ := int.
-Notation "P ∨ Q" := (P + Q)%type.
-
-Module IntToZmod7.
 
 Definition Rp := SplitSurj.toParam (SplitSurj.Build reprpK).
 
 Axiom Rzero : Rp zero zerop.
 Axiom Rone : Rp one onep.
+Variable Rmod3 : unop_param Rp Rp mod3 modp3.
 Variable Radd : binop_param Rp Rp Rp add addp.
 Variable Rmul : binop_param Rp Rp Rp mul mulp.
-Variable Reqmodp01 : forall (m : int) (x : Zmod7), Rp m x ->
-  forall n y, Rp n y -> Param01.Rel (eqmodp m n) (eq_Zmod7 x y).
+Variable Reqmodp01 : forall (m : int) (x : Zmod9), Rp m x ->
+  forall n y, Rp n y -> Param01.Rel (eqmodp m n) (eq_Zmod9 x y).
 
-Trocq Use Rp Rmul Rzero Rone Param10_paths Reqmodp01.
-Trocq Use Param01_sum.
 
-Lemma IntRedModZp : forall (m n p : ℤ),
-  m = n²%ℤ -> m = p³%ℤ  -> m ≡ 0 ∨ m ≡ 1.
+
+Trocq Use Rp Rmul Rzero Rone Radd Rmod3 Param10_paths Reqmodp01.
+Trocq Use Param01_sum Param01_Empty Param10_Empty.
+
+Lemma flt3_step : forall (m n p : ℤ),
+  m * n * p % 3 ≢ 0 -> (m³ + n³)%ℤ  ≠ p³%ℤ.
 Proof.
 trocq=> /=.
 Admitted.
-
-(* Print Assumptions IntRedModZp. (* No Univalence *) *)
-
-End IntToZmod7.
-
-Module Zmod7ToInt.
-
-Definition Rp := SplitSurj.toParamSym (SplitSurj.Build reprpK).
-
-Axiom Rzero : Rp zerop zero.
-Variable Radd : binop_param Rp Rp Rp addp add.
-Variable paths_to_eqmodp : binop_param Rp Rp iff paths eqmodp.
-
-Trocq Use Rp Param01_paths Param10_paths Radd Rzero.
-
-Goal (forall x y, x + y = y + x)%Zmod7.
-Proof.
-  trocq.
-  exact addC.
-Qed.
-
-Goal (forall x y z, x + y + z = y + x + z)%Zmod7.
-Proof.
-  intros x y z.
-  suff addpC: forall x y, (x + y = y + x)%Zmod7. {
-    by rewrite (addpC x y). }
-  trocq.
-  exact addC.
-Qed.
-
-End Zmod7ToInt.
