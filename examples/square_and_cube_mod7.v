376259545af15369e61b8a8b2cf702ab5ac1a248
Cyril Cohen <cohen@crans.org>
2024-10-18 11:41:11 +0200

    adding square and cube example, with a cheat

--- examples/square_and_cube_mod7.v
+++ examples/square_and_cube_mod7.v
@@ -24,9 +24,9 @@ Declare Scope int_scope.
 Delimit Scope int_scope with int.
 Delimit Scope int_scope with ℤ.
 Local Open Scope int_scope.
-Declare Scope Zmod9_scope.
-Delimit Scope Zmod9_scope with Zmod9.
-Local Open Scope Zmod9_scope.
+Declare Scope Zmod7_scope.
+Delimit Scope Zmod7_scope with Zmod7.
+Local Open Scope Zmod7_scope.
 
 (* Helper predicates *)
 Definition unop_param {X X'} RX {Y Y'} RY
@@ -39,33 +39,32 @@ Definition binop_param {X X'} RX {Y Y'} RY {Z Z'} RZ
 
 (* For now translations need the support of monomorphic identifiers: *)
 
-(* Arithmetic on Z_9*)
-Definition Zmod9 := 'Z_9.
-Definition zerop : Zmod9 := Zp0.
-Definition addp : Zmod9 -> Zmod9 -> Zmod9 := @Zp_add 8.
-Definition mulp : Zmod9 -> Zmod9 -> Zmod9 := @Zp_mul 8.
-Definition onep : Zmod9 := Zp1.
+(* Arithmetic on Z_7*)
+Definition Zmod7 := 'Z_7.
+Notation "ℤ/7ℤ" := Zmod7.
+
+Definition zerop : ℤ/7ℤ := @Zp0 6.
+Definition addp : ℤ/7ℤ -> ℤ/7ℤ -> ℤ/7ℤ := @Zp_add 6.
+Definition mulp : ℤ/7ℤ -> ℤ/7ℤ -> ℤ/7ℤ := @Zp_mul 6.
+Definition onep : ℤ/7ℤ := Zp1.
 
 (* Quotient *)
-Definition modp : int -> Zmod9 := fun x => (x)%:~R.
-Definition reprp : Zmod9 -> int := idmap.
+Definition modp : int -> ℤ/7ℤ := fun x => (x)%:~R.
+Definition reprp : ℤ/7ℤ -> int := idmap.
 
 Lemma reprpK : forall x, modp (reprp x) = x. Proof. exact: natr_Zp. Qed.
 
-(* Mod 3 in Z9 *)
-Lemma mk_mod9_mod3 (n : 'I_9) : (n %% 3 < 9)%N.
+(* Mod 3 in Z7 *)
+Lemma mk_mod7_mod3 (n : 'I_7) : (n %% 3 < 7)%N.
 Proof. apply: (@ltn_trans 3) => //; exact: ltn_pmod. Qed.
 
-Definition modp3 : Zmod9 -> Zmod9 := fun n => Ordinal (mk_mod9_mod3 n).
+Definition modp3 : ℤ/7ℤ -> ℤ/7ℤ := fun n => Ordinal (mk_mod7_mod3 n).
 
 (* Equality on integers *)
 Definition eqmodp (x y : int) := modp x = modp y.
 
-Definition eq_Zmod9 (x y : Zmod9) := (x = y).
-Arguments eq_Zmod9 /.
-
-(* Definition mod3 : int -> int := TODO *)
-Definition mod3 : int -> int := (fun x => x %% 3).
+Definition eq_Zmod7 (x y : ℤ/7ℤ) := (x = y).
+Arguments eq_Zmod7 /.
 
 Definition zero : int := 0.
 Definition one : int := 1.
@@ -74,26 +73,25 @@ Definition add : int -> int -> int := fun x y => x + y.
 Definition mul : int -> int -> int := fun x y => x * y.
 
 Notation "0" := zero : int_scope.
-Notation "0" := zerop : Zmod9_scope.
+Notation "0" := zerop : Zmod7_scope.
 Notation "1" := one : int_scope.
-Notation "1" := onep : Zmod9_scope.
+Notation "1" := onep : Zmod7_scope.
 Notation "x + y" := (add x%int y%int) : int_scope.
-Notation "x + y" := (addp x%Zmod9 y%Zmod9) : Zmod9_scope.
+Notation "x + y" := (addp x%Zmod7 y%Zmod7) : Zmod7_scope.
 Notation "x * y" := (mul x%int y%int) : int_scope.
-Notation "x * y" := (mulp x%Zmod9 y%Zmod9) : Zmod9_scope.
+Notation "x * y" := (mulp x%Zmod7 y%Zmod7) : Zmod7_scope.
 Notation not A := (A -> Empty).
-Notation "m ^ 2" := (m * m)%int (at level 2) : int_scope.
-Notation "m ^ 2" := (m * m)%Zmod9 (at level 2) : Zmod9_scope.
+Notation "m ²" := (m * m)%int (at level 2) : int_scope.
+Notation "m ²" := (m * m)%Zmod7 (at level 2) : Zmod7_scope.
 Notation "m ³" := (m * m * m)%int (at level 2) : int_scope.
-Notation "m ³" := (m * m * m)%Zmod9 (at level 2) : Zmod9_scope.
-Notation "m % 3" := (mod3 m)%int (at level 2) : int_scope.
-Notation "m % 3" := (modp3 m)%Zmod9 (at level 2) : Zmod9_scope.
+Notation "m ³" := (m * m * m)%Zmod7 (at level 2) : Zmod7_scope.
+Notation "m % 3" := (modp3 m)%Zmod7 (at level 2) : Zmod7_scope.
 Notation "x ≡ y" := (eqmodp x%int y%int)
   (format "x  ≡  y", at level 70) : int_scope.
 Notation "x ≢ y" := (not (eqmodp x%int y%int))
   (format "x  ≢  y", at level 70) : int_scope.
 Notation "x ≠ y" := (not (x = y)) (at level 70).
-Notation "ℤ/9ℤ" := Zmod9.
+Notation "A ∨ B" := ((not A) -> B) (at level 85) : type_scope.
 Notation ℤ := int.
 
 Definition Rp := SplitSurj.toParam (SplitSurj.Build reprpK).
@@ -103,7 +101,7 @@ Lemma Rzero : Rp zero zerop. Proof. done. Qed.
 Lemma Rone : Rp one onep. Proof. done. Qed.
 
 
-(* These lemmas are about congruence mod 9, of + and * *)
+(* These lemmas are about congruence mod 7, of + and * *)
 Lemma Radd : binop_param Rp Rp Rp add addp.
 Proof.
 rewrite /Rp /= /graph /==> x1 y1 <- x2 y2 <-.
@@ -116,41 +114,8 @@ rewrite /Rp /= /graph /= => x1 y1 <- x2 y2 <-.
 by rewrite /modp rmorphM.
 Qed.
 
-(* This lemma says that mod-ing by 3 is the same as first mod-ing by 9 and then
-   mod-ing by 3. Can probably be streamlined... *)
-Lemma Rmod3 : unop_param Rp Rp mod3 modp3.
-Proof.
-rewrite /Rp /= /graph /= => x y <- {y}.
-rewrite /modp /mod3 /modp3.
-apply: val_inj=> /=.
-wlog : x / exists n, x = Posz n.
-  move=> hwlog.
-  case: x => n.
-  - by apply: hwlog; exists n.
-  - rewrite modNz_nat //.
-    rewrite -[Posz (1 +  _)%N - 1]/(2%int) -[(1 + _)%N]/(3%N) NegzE rmorphN /=.
-    rewrite -[_.+2]/(9%N) modn_dvdm //.
-    rewrite modnB //=; last by rewrite [_%:~R]Zp_nat ltnW.
-    set x : 'I_9 := _.+1%:~R.
-    rewrite -[(9 %% 3)%N]/(0%N) addn0.
-    have {x} -> : x = (n.+1 %% 9)%N :> nat by rewrite {}/x [_%:~R]Zp_nat //.
-    rewrite modn_dvdm // modnS; case: ifP => //= hn.
-    - by rewrite mul0n subn0 -[n]/(n.+1.-1) modn_pred // hn.
-    - rewrite -[n]/(n.+1.-1) modn_pred // hn mul1n subSS subzn; last first.
-        by apply: (@leq_trans (n.+1 %% 3)); rewrite ?leq_pred // -ltnS ltn_pmod.
-      rewrite [_%:~R]Zp_nat [LHS]modn_small //=.
-      by apply: (@leq_trans 3) => //; rewrite ltnS leq_subr.
-case=> n -> {x}.
-set u : 'I_9 := _%:~R; set v := (X in (X %% 3)%N).
-have {v} -> : v = (n %% 9)%N by exact: val_Zp_nat.
-rewrite modn_dvdm //.
-have -> : nat_of_ord u = ((n %% 3)%N %% 9)%N.
-  by rewrite {}/u modz_nat /= (@val_Zp_nat 9).
-rewrite modn_small //; apply: (@ltn_trans 3) => //; exact: ltn_pmod.
-Qed.
-
-Lemma Reqmodp01 : forall (m : int) (x : Zmod9), Rp m x ->
-  forall n y, Rp n y -> Param01.Rel (eqmodp m n) (eq_Zmod9 x y).
+Lemma Reqmodp01 : forall (m : int) (x : ℤ/7ℤ), Rp m x ->
+  forall n y, Rp n y -> Param01.Rel (eqmodp m n) (eq_Zmod7 x y).
 Proof.
 rewrite /Rp /= /graph /=.
 move=> x k exk y l eyl.
@@ -158,24 +123,23 @@ apply: (@Param01.BuildRel (x ≡ y) (k = l) (fun _ _ => unit)) => //.
 by constructor; rewrite -exk -eyl.
 Qed.
 
-Trocq Use Rp Rmul Rzero Rone Radd Rmod3 Param10_paths Reqmodp01.
-Trocq Use Param01_sum.
+Trocq Use Rp Rmul Rzero Rone Radd Param10_paths Reqmodp01.
 Trocq Use Param01_Empty.
 Trocq Use Param10_Empty.
 
-Lemma flt3_step : forall (m n p : ℤ),
-  ((m * n * p)%Z % 3)%Z ≢ 0 -> (m³ + n³)%ℤ  ≠ p³%ℤ.
+Lemma square_and_cube_mod7 : forall (m n p : ℤ),
+  (m = n²)%Z -> (m = p³)%Z -> m ≡ 0 ∨ m ≡ 1.
 Proof.
-trocq=> /=.
+trocq => /=.
 
 (* the following should be a call to troq to compute *)
-move=> + + + /eqP + /eqP.
-by do 3![case; do 9?[case=> //=] => ?].
+move=> m n p /eqP mn /eqP mp /eqP m0. apply/eqP; move: m n p mn mp m0.
+do 3![case; do 7?[case=> //=] => ?] => /=.
 
 (* Ultimately, it should be:
 
-by rewrite (mod 9); decide.
+by rewrite (mod 7); decide.
 
 *)
 Qed.
-Print Assumptions flt3_step.
+Print Assumptions square_and_cube_mod7.
